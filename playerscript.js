50418bd68cb4526dc5ffb2a85f266fb5ca629216
LentoLen <lenn.vnn@gmail.com>
2023-08-11 19:43:32 +0200

    modified:   index.html 	modified:   playerscript.js 	modified:   scriptytm.js 	modified:   style.css

--- playerscript.js
+++ playerscript.js
@@ -1,6 +1,8 @@
 let player;
 let videoQueue = [];
-let currentVideoId 
+let autoQueue = {}
+let history = []
+let currentVideoId;
 
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
@@ -36,68 +38,156 @@ function onPlayerStateChange(event) {
 }
 
 function playNextVideo() {
-    let video
+    let video;
 
     if (videoQueue.length > 0) {
       video = videoQueue.shift();
-      updatePlayer(video["videoId"], video["thumbnail"], video["title"], video["artist"]);
+    } else if (autoQueue["videoId"]){
+        video = autoQueue;
     } else {
-        getAutoplayNext()
-            .then(data => {
-                updatePlayer(data["videoId"], data["thumbnail"], data["title"], data["artists"][0]["name"]);
-            })
-            .catch(error => {
-                // Handle any errors that occur during the fetch request
-                console.error("Error fetching autoplay data:", error);
-            });
+        getAutoplay();
     }
+    history.push(video);
+    updatePlayer(video["videoId"], video["thumbnail"], video["title"], video["artist"]);
 }
 
 function updatePlayer(videoId, thumbnail, title, artist) {
-    document.getElementById("bg_img").style.backgroundImage = (`url(${thumbnail})`)
-    console.log(thumbnail)
-    player.loadVideoById(videoId)
-    player.getIframe().style.display = "block"
-    document.getElementById("player_controls").style.visibility = "visible"
-    document.getElementById("queue_lyrics_div").style.display= "block"
-    document.getElementById("vid_info").innerText = `${title} - ${artist}`
-    currentVideoId = videoId
-    getLyrics()
+    document.getElementById("bg_img").style.backgroundImage = (`url(${thumbnail})`);
+    console.log(thumbnail);
+    player.loadVideoById(videoId);
+    player.getIframe().style.display = "block";
+    document.getElementById("player_controls").style.visibility = "visible";
+    document.getElementById("queue_lyrics_div").style.display= "block";
+    document.getElementById("vid_info").innerText = `${title} - ${artist}`;
+    currentVideoId = videoId;
+    getAutoplay();
+    getLyrics();
+    showQueueNext();
+    showHistory();
+    scrollToNextQueue();
 }
 
 function addToQueue(videoId, thumbnail, title, artist) {
     videoQueue.push({"videoId": videoId, "thumbnail": thumbnail, "title": title, "artist": artist});
-
+    
     if (player.getPlayerState() === 5) {
-        playNextVideo()
+        playNextVideo();
     } else {
-        showSnackbar(`added "${title} - ${artist}" to queue`)
+        showSnackbar(`added "${title} - ${artist}" to queue`);
+    }
+    showQueueNext();
+    scrollToNextQueue();
+}
+
+function playFromQueue(index) {
+    let video = videoQueue.splice(index, 1)[0];
+    history.push(video);
+    updatePlayer(video["videoId"], video["thumbnail"], video["title"], video["artist"]);
+}
+
+function playFromHistory(index) {
+    let video = history[index];
+    history.push(video);
+    updatePlayer(video["videoId"], video["thumbnail"], video["title"], video["artist"]);
+}
+
+function showQueueNext() {
+    if (videoQueue.length > 0) {
+        document.getElementById("queue_next").innerHTML = "<p>queue</p>";
+        videoQueue.forEach((element, i) => {
+            document.getElementById("queue_next").innerHTML += `<div class="queue_item" onclick="playFromQueue(${i})"><img src="${element["thumbnail"]}" class="queue_item_thumbnail"></img><div class="queue_item_details"><div class="queue_item_title">${element["title"]}</div><div class="queue_item_artist">${element["artist"]}</div></div><div class="material-symbols-outlined queue_item_dismiss" onclick="dismissQueue(event, ${i})">close</div>`;
+        });
+    } else {
+        document.getElementById("queue_next").innerHTML = "";
+    }
+    
+}
+
+function addAutoplayToQueue() {
+    addToQueue(autoQueue["videoId"], autoQueue["thumbnail"], autoQueue["title"], autoQueue["artist"]);
+    getAutoplay();
+}
+
+function showHistory() {
+    if (history.length > 1) {
+        document.getElementById("queue_history").innerHTML = "<p>history</p>";
+        history.slice(0, history.length - 1).forEach((element, i) => {
+            document.getElementById("queue_history").innerHTML += `<div class="queue_item" onclick="playFromHistory(${i})" style="opacity:0.7;"><img src="${element["thumbnail"]}" class="queue_item_thumbnail"></img><div class="queue_item_details"><div class="queue_item_title">${element["title"]}</div><div class="queue_item_artist">${element["artist"]}</div></div><div class="material-symbols-outlined queue_item_dismiss" onclick="dismissHistory(event, ${i})">close</div>`;
+        });
+    } else {
+        document.getElementById("queue_history").innerHTML = "";
     }
 }
 
-function getAutoplayNext() {
-    const autoplayUrl = `${apiUrl}/autoplay/?videoId=${encodeURIComponent(currentVideoId)}`
-    return fetch(autoplayUrl, getJsonHeaders)
+function dismissQueue(e, index) {
+    e.stopPropagation();
+    videoQueue.splice(index, 1);
+    showQueueNext();
+}
+
+function dismissHistory(e, index) {
+    e.stopPropagation();
+    history.splice(index, 1);
+    showHistory();
+}
+
+function dismissAutoplay(e) {
+    e.stopPropagation();
+    getAutoplay();
+}
+
+function scrollToNextQueue() {
+    if (document.getElementById("queue_next").innerHTML != "") {
+        document.getElementById("queue_div").scrollTop = document.getElementById("queue_next").offsetTop;
+    } else {
+        document.getElementById("queue_div").scrollTop = document.getElementById("queue_autoplay").offsetTop;
+    }
+}
+
+function getAutoplay() {
+    document.getElementById("queue_autoplay").innerHTML = `<p>autoplay</p><div class="queue_item">loading...</div>`;
+    const autoplayUrl = `${apiUrl}/autoplay/?videoId=${encodeURIComponent(currentVideoId)}`;
+    fetch(autoplayUrl, getJsonHeaders)
         .then(response => response.json())
+        .then(data => {
+            autoQueue = {"videoId": data["videoId"], "thumbnail": data["thumbnail"], "title": data["title"], "artist": data["artists"][0]["name"]};
+            document.getElementById("queue_autoplay").innerHTML = `<p>autoplay</p><div class="queue_item" onclick="addAutoplayToQueue()"><img src="${autoQueue["thumbnail"]}" class="queue_item_thumbnail"></img><div class="queue_item_details"><div class="queue_item_title">${autoQueue["title"]}</div><div class="queue_item_artist">${autoQueue["artist"]}</div></div><div class="material-symbols-outlined queue_item_dismiss" onclick="dismissAutoplay(event)">close</div>`;
+        })
+        .catch(error => {
+            showSnackbar("Couldn't load autoplay title...");
+            console.error("Error fetching autoplay data:", error);
+        });
 }
 
 function getLyrics() {
+    document.getElementById("lyrics").innerHTML = `loading...`;
     const lyricsUrl = `${apiUrl}/lyrics/?videoId=${encodeURIComponent(currentVideoId)}`
     fetch(lyricsUrl, getJsonHeaders)
         .then(response => response.json())
         .then(data => {
-            document.getElementById("lyrics").innerHTML = `${data["lyrics"]}<br>${data["source"]}`
+            document.getElementById("lyrics").innerHTML = `${data["lyrics"]}<br>${data["source"]}`;
     })
+}
+
+function playPrevious() {
+    if (history.length > 1) {
+        videoQueue.unshift(history.pop());
+        let video = history[history.length-1];
+        updatePlayer(video["videoId"], video["thumbnail"], video["title"], video["artist"]);
+    } else {
+        showSnackbar("no titles in history");
+    }
+    
 }
 
 function playPause() {
     switch (player.getPlayerState()) {
         case YT.PlayerState.PLAYING:
-            player.pauseVideo()
+            player.pauseVideo();
             break;
 
         case YT.PlayerState.PAUSED:
-            player.playVideo()
+            player.playVideo();
     
         default:
             break;
--- scriptytm.js
+++ scriptytm.js
@@ -1,5 +1,9 @@
 const searchInput = document.getElementById("searchInput")
 const searchSubmit = document.getElementById("searchSubmit")
+const lyricsDiv = document.getElementById('lyrics_div');
+const queueDiv = document.getElementById('queue_div');
+const lyricsTab = document.getElementById('tab_lyrics');
+const queueTab = document.getElementById('tab_queue');
 const snack = document.getElementById("snackbar")
 const apiUrl = "https://musicpartyv2-1-k0554251.deta.app";
 const getJsonHeaders = {
@@ -11,19 +15,19 @@ const getJsonHeaders = {
 
 function validateInput() {
     const inputValue = searchInput.value.trimStart().replace(/\s\s+/g, ' ');
-    searchInput.value = inputValue
+    searchInput.value = inputValue.substring(0,120);
 }
 
 function search() {
-    const inp = searchInput.value
+    const inp = searchInput.value;
     if (inp == "") {
         return
     }
-    searchInput.disabled = true
-    searchSubmit.className += "spin"
-    suggestions_div.innerText = ""
+    searchInput.disabled = true;
+    searchSubmit.className += "spin";
+    suggestions_div.innerText = "";
 
-    const searchUrl = `${apiUrl}/search/?query=${encodeURIComponent(inp)}&limit=1`
+    const searchUrl = `${apiUrl}/search/?query=${encodeURIComponent(inp)}&limit=1`;
 
     fetch(searchUrl, getJsonHeaders)
    .then(response => response.json())
@@ -31,34 +35,50 @@ function search() {
         if (data.videos.length > 0) {
             const videoId = data.videos[0].videoId;
             const thumbnail = data.videos[0].thumbnail;
-            const title = data.videos[0].title
-            const artist = data.videos[0].artists[0].name
-            addToQueue(videoId, thumbnail, title, artist)
-            resetSearchbar()
+            const title = data.videos[0].title;
+            const artist = data.videos[0].artists[0].name;
+            addToQueue(videoId, thumbnail, title, artist);
+            resetSearchbar();
+        } else {
+            showSnackbar(`"${inp}" not found`);
+            resetSearchbar();
         }
     })
     .catch((error) => {
         console.error("Error fetching data:", error);
-        showSnackbar("network error... try again")
-        resetSearchbar()
+        showSnackbar("network error... try again");
+        resetSearchbar();
     });
 }
 
 function resetSearchbar() {
-    searchSubmit.className = searchSubmit.className.replace("spin", "")
-    searchInput.value = ""
-    searchInput.disabled = false
-    suggestions_div.innerText = ""
+    searchSubmit.className = searchSubmit.className.replace("spin", "");
+    searchInput.value = "";
+    searchInput.disabled = false;
 }
 
 function showSnackbar(message, timeout=3000) {
-    snack.innerText = message
-    snack.className = "show"
+    snack.innerText = message;
+    snack.className = "show";
     setTimeout(function(){ snack.className = snack.className.replace("show", ""); }, timeout);
+}
+
+function showLyricsTab() {
+    lyricsDiv.classList.add('active');
+    queueDiv.classList.remove('active');
+    lyricsTab.classList.add('active');
+    queueTab.classList.remove('active');
+}
+
+function showQueueTab() {
+    queueDiv.classList.add('active');
+    lyricsDiv.classList.remove('active');
+    queueTab.classList.add('active');
+    lyricsTab.classList.remove('active');
 }
 
 searchInput.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
-        search()
+        search();
     }
 }); 
