138bbfaa27f9458355b7e314717720f2ff97abb8
LentoLen <lenn.vnn@gmail.com>
2023-09-17 03:42:01 +0200

    modified:   index.html 	modified:   playerscript.js 	modified:   style.css

--- playerscript.js
+++ playerscript.js
@@ -63,6 +63,7 @@ function updatePlayer(videoId, thumbnail, title, artist) {
     getAutoplay();
     getLyrics();
     showQueueNext();
+    showPlaying();
     showHistory();
     scrollToNextQueue();
 }
@@ -93,12 +94,36 @@ function playFromHistory(index) {
 
 function showQueueNext() {
     if (videoQueue.length > 0) {
-        document.getElementById("queue_next").innerHTML = "<p>queue</p>";
+        document.getElementById("queue_next").innerHTML = "<p style='user-select: none;'>queue</p>";
         videoQueue.forEach((element, i) => {
             document.getElementById("queue_next").innerHTML += `<div class="queue_item" onclick="playFromQueue(${i})"><img src="${element["thumbnail"]}" class="queue_item_thumbnail"></img><div class="queue_item_details"><div class="queue_item_title">${element["title"]}</div><div class="queue_item_artist">${element["artist"]}</div></div><div class="material-symbols-outlined queue_item_dismiss" onclick="dismissQueue(event, ${i})">close</div>`;
         });
     } else {
         document.getElementById("queue_next").innerHTML = "";
+    }
+    
+}
+
+function showPlaying() {
+    console.log("playing", history[0]["title"])
+    if (history.length > 0) {
+        const vidPlaying = history[history.length-1]
+        const videoId = vidPlaying["videoId"]
+        const title = vidPlaying["title"]
+        document.getElementById("queue_playing").innerHTML = "<p style='user-select: none;'>playing</p>";
+        document.getElementById("queue_playing").innerHTML += `<div class="queue_item" onclick="playPause()"><img src="${vidPlaying["thumbnail"]}" class="queue_item_thumbnail"></img><div class="queue_item_details"><div class="queue_item_title">${vidPlaying["title"]}</div><div class="queue_item_artist">${vidPlaying["artist"]}</div></div><div id="dl-btn" style="padding-top:20px; padding-bottom:20px; height:fit-content;" class="material-symbols-outlined queue_item_dismiss">download</div>`;
+        const dlBtn = document.getElementById("dl-btn")
+        dlBtn.addEventListener("click", function (event) {
+            dlBtn.style.color = "var(--secBg)"
+            dlBtn.innerHTML = "progress_activity"
+            dlBtn.style.animation = "spin 1.2s cubic-bezier(.25,.41,.68,.55) infinite"
+            dlBtn.style.pointerEvents = "none"
+            download_audio(videoId, title)
+            event.stopPropagation()
+        })
+        
+    } else {
+        document.getElementById("queue_playing").innerHTML = "";
     }
     
 }
@@ -110,13 +135,30 @@ function addAutoplayToQueue() {
 
 function showHistory() {
     if (history.length > 1) {
-        document.getElementById("queue_history").innerHTML = "<p>history</p>";
+        document.getElementById("queue_history").innerHTML = "<p style='user-select: none;'>history</p>";
         history.slice(0, history.length - 1).forEach((element, i) => {
             document.getElementById("queue_history").innerHTML += `<div class="queue_item" onclick="playFromHistory(${i})" style="opacity:0.7;"><img src="${element["thumbnail"]}" class="queue_item_thumbnail"></img><div class="queue_item_details"><div class="queue_item_title">${element["title"]}</div><div class="queue_item_artist">${element["artist"]}</div></div><div class="material-symbols-outlined queue_item_dismiss" onclick="dismissHistory(event, ${i})">close</div>`;
         });
     } else {
         document.getElementById("queue_history").innerHTML = "";
     }
+}
+
+function download_audio(videoId, title) {
+    fetch(`https://ytdapi.onrender.com/download-audio/?video_id=${videoId}`)
+        .then(response => response.blob())
+        .then(blob => {
+            const audioUrl = window.URL.createObjectURL(blob)
+            const dlLink = document.getElementById("dl-link")
+            dlLink.href = audioUrl
+            dlLink.download = `${title}.m4a`
+            dlLink.click()
+            const dlBtn = document.getElementById("dl-btn")
+            dlBtn.style.animation = "none"
+            dlBtn.innerHTML = "download"
+        })
+    
+    
 }
 
 function dismissQueue(e, index) {
@@ -145,13 +187,13 @@ function scrollToNextQueue() {
 }
 
 function getAutoplay() {
-    document.getElementById("queue_autoplay").innerHTML = `<p>autoplay</p><div class="queue_item">loading...</div>`;
+    document.getElementById("queue_autoplay").innerHTML = `<p style="user-select: none;">autoplay</p><div class="queue_item">loading...</div>`;
     const autoplayUrl = `${apiUrl}/autoplay/?videoId=${encodeURIComponent(currentVideoId)}`;
     fetch(autoplayUrl, getJsonHeaders)
         .then(response => response.json())
         .then(data => {
             autoQueue = {"videoId": data["videoId"], "thumbnail": data["thumbnail"], "title": data["title"], "artist": data["artists"][0]["name"]};
-            document.getElementById("queue_autoplay").innerHTML = `<p>autoplay</p><div class="queue_item" onclick="addAutoplayToQueue()"><img src="${autoQueue["thumbnail"]}" class="queue_item_thumbnail"></img><div class="queue_item_details"><div class="queue_item_title">${autoQueue["title"]}</div><div class="queue_item_artist">${autoQueue["artist"]}</div></div><div class="material-symbols-outlined queue_item_dismiss" onclick="dismissAutoplay(event)">close</div>`;
+            document.getElementById("queue_autoplay").innerHTML = `<p style="user-select: none;">autoplay</p><div class="queue_item" onclick="addAutoplayToQueue()"><img src="${autoQueue["thumbnail"]}" class="queue_item_thumbnail"></img><div class="queue_item_details"><div class="queue_item_title">${autoQueue["title"]}</div><div class="queue_item_artist">${autoQueue["artist"]}</div></div><div class="material-symbols-outlined queue_item_dismiss" onclick="dismissAutoplay(event)">close</div>`;
         })
         .catch(error => {
             showSnackbar("Couldn't load autoplay title...");
